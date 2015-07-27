958745033ce29fe6353c007f3a902ad15fcbd01c
Shinji Yoshida <yoshida.shinji.gm@gmail.com>
2015-07-27 11:12:03 +0900

    update

---
+++ src/Lockables/Lock.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+namespace Lockables {
+	public class Lock : IDisposable {
+		ILockable lockable;
+
+		public Lock (ILockable lockable) {
+			this.lockable = lockable;
+			lockable.Lock();
+		}
+		
+		public void Dispose () {
+			if(lockable == null)
+				return;
+
+			lockable.Unlock();
+			lockable = null;
+		}
+	}
+}
--- src/Lockables/Lockable.cs
+++ src/Lockables/Lockable.cs
@@ -8,6 +8,23 @@ namespace Lockables {
 		public static ILockable Create(Action onLocked, Action onUnlocked) {
 			return new SimpleLockable(onLocked, onUnlocked);
 		}
+
+		public static ILockable Composite(params ILockable[] lockables) {
+			var result = new CompositeLockable();
+			foreach(var each in lockables)
+				result.Add(each);
+			return result;
+		}
+
+		public static IDisposable WithLock(this ILockable lockable, Action<IDisposable> action) {
+			var singleLock = new Lock(lockable);
+			action(singleLock);
+			return singleLock;
+		}
+
+		public static IDisposable Lock(this ILockable lockable) {
+			return new Lock(lockable);
+		}
 	}
 
 }
