4b7556dec438d8fa55a4861d202de73dc00f1eb3
Shinji Yoshida <yoshida.shinji.gm@gmail.com>
2019-06-11 10:32:51 +0900

    KeywordLock

---
+++ src/Lockables/KeywordLock.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+
+namespace Lockables
+{
+	public class KeywordLock
+	{
+		private HashSet<string> lockingKeyword = new HashSet<string>();
+		private ILockable lockable;
+
+		public KeywordLock(ILockable lockable)
+		{
+			this.lockable = lockable;
+		}
+
+		public bool IsLocked
+		{
+			get { return lockable.IsLocked(); }
+		}
+
+		public void Lock(string keyword)
+		{
+			if (lockingKeyword.Count == 0) {
+				lockingKeyword.Add(keyword);
+				lockable.Lock();
+			}
+			else {
+				lockingKeyword.Add(keyword);
+			}
+		}
+
+		public void Unlock(string keyword)
+		{
+			if(lockingKeyword.Count == 0)
+				return;
+			;
+			lockingKeyword.Remove(keyword);
+			if(lockingKeyword.Count == 0)
+				lockable.Unlock();
+		}
+	}
+}
